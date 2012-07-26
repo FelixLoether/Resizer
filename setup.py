8c0456bc356ac9683095f7624d4465339f78a644
Oskari Hiltunen <felix.loether@gmail.com>
2012-07-26 16:47:07 +0300

    Add various items to setup.py.

--- setup.py
+++ setup.py
@@ -18,8 +18,12 @@ class PyTest(Command):
 setup(
     name='Resizer',
     version='0.0.1',
+    url='http://github.com/FelixLoether/resizer',
+    author='Oskari Hiltunen',
+    author_email='resizer@loethr.net',
     description='Resizer helps you generate thumbnails and '
                 'load images from various sources.',
+    long_description=open('README.md').read(),
     packages=['resizer'],
     platforms='any',
     install_requires=['Pillow>=1.7.7'],
